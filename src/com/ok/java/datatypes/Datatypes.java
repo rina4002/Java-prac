848e37109c650e803c2a5de1787cc1eec650e638
Rinal Achkarpohre <rinal12achkarpohre@gmail.com>
2025-10-19 14:46:06 +0530

    ADD datatype

---
+++ src/com/ok/java/datatypes/Datatypes.java
@@ -0,0 +1,18 @@
+package com.ok.java.datatypes;
+
+public class Datatypes {
+    public static void main (String[] args ){
+        System.out.println("ok");
+        int a = 1;
+        byte b = 1;
+        short c = 1;
+        long d = 1;
+        float e = 1.2f;
+        double f = 1.33;
+        boolean value = true ;
+
+        char g = 'n';
+
+        System.out.println ( c+d );
+    }
+}
